759bb19bdd12106c059fb8931d5d339abf44f2d3
Fernando Barbalho <fbarbalho@hotmail.com>
2024-01-26 12:24:54 -0300

    Cechagem da qualidade do dado e estatísticas descritivas

--- analises_exploratorias.R
+++ analises_exploratorias.R
@@ -28,11 +28,20 @@ dados_orcamento_2023%>%
   geom_boxplot(fill=NA, outlier.shape = NA) +
   geom_jitter() 
 
+
+
 dados_orcamento_2018_2023 %>%
   mutate(pago_empenhado = (pago/empenhado)*100)%>%
   ggplot(aes(x=ano, y=pago_empenhado)) +
   geom_jitter() +  
   geom_boxplot(fill=NA,  outlier.shape = NA, color= "red") 
+
+summary(dados_orcamento_2018_2023)
+
+fab<-
+  dados_orcamento_2018_2023 %>%
+  filter(pago > dotacao_atual)
+  
 
 dados_orcamento_2018_2023 %>%
   filter(empenhado>0) %>%
@@ -43,23 +52,43 @@ dados_orcamento_2018_2023 %>%
 
 
 dados_orcamento_2018_2023 %>%
+  filter(dotacao_atual >0 ) %>%
+  summarise(pago= sum(pago),
+            dotacao_atual = sum(dotacao_atual),
+            .by= orgao_orcamentario) %>% 
   mutate(pago_dotacao = (pago/dotacao_atual)*100)%>%
-  summarise(mediana = median(pago_empenhado),
-            .by= ano)
+  summarise(mediana = median(pago_dotacao),
+            media = mean(pago_dotacao))
+
+dados_orcamento_2018_2023 %>%
+  filter(dotacao_atual >0,
+         ano == 2023) %>%
+  mutate(pago_dotacao = (pago/dotacao_atual)*100)%>%
+  summarise(mediana = median(pago_dotacao),
+            media = mean(pago_dotacao))
+
+
+
+
+
 
 summary(fab)
 
 fab<-
   dados_orcamento_2023%>%
-  filter(dotacao >0 ) %>%
-  mutate(pago_dotacao = (empenhado/dotacao_atual)*100) %>%
+  mutate(pago_dotacao = (empenhado/dotacao_atual)*100) 
+
   
+fab %>%
+  filter(dotacao_atual == 0)
   
 
 dados_orcamento_2023%>%
   mutate(pago_dotacao = (empenhado/dotacao_atual)*100) %>%
   summarise(mean(pago_dotacao),
             median(pago_dotacao))
+
+
 
 
 dados_orcamento_2023%>%
@@ -84,3 +113,9 @@ modelo<- lm(log(pago)~log(dotacao_atual), data = dados_orcamento_2023[,])
 
 
 performance::check_model(modelo)
+
+fab<-
+dados_orcamento_2018_2023 %>% 
+  mutate(pago_dotacao = (pago/dotacao_atual)*100) %>%
+  inner_join(df_de_para) %>%
+  filter(ano==2023)
